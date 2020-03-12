b35436c95ed5a0cf2113a58d0a7325e98ab05b78
BaxLi <lilian.baxan@gmail.com>
2020-03-12 21:44:43 +0200

    r

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 
 //-------- for HEROKU deployment --------------------------------------------------
 const PORT = process.env.PORT
-console.log("config.get('port')=", process.env.PORT );
+console.log("config.get('port')=", process.env.PORT )
 //---------------------------------------------------------------------------------
 
 async function start() {
